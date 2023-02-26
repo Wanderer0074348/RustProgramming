78c725aecf4c8d725cda9669c5e77fb758f61f45
Tanay Matta <83384365+Wanderer0074348@users.noreply.github.com>
2023-02-26 11:58:58 +0400

    Made a Rust Initialized Directory

---
+++ target/debug/deps/RustProgramming-2ab29b6f29543da9.d
@@ -0,0 +1,5 @@
+/home/tanay/Documentos/RustProgramming/target/debug/deps/RustProgramming-2ab29b6f29543da9.rmeta: src/main.rs
+
+/home/tanay/Documentos/RustProgramming/target/debug/deps/RustProgramming-2ab29b6f29543da9.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/RustProgramming-b1d48f54fbd49567.d
@@ -0,0 +1,5 @@
+/home/tanay/Documentos/RustProgramming/target/debug/deps/RustProgramming-b1d48f54fbd49567.rmeta: src/main.rs
+
+/home/tanay/Documentos/RustProgramming/target/debug/deps/RustProgramming-b1d48f54fbd49567.d: src/main.rs
+
+src/main.rs:
